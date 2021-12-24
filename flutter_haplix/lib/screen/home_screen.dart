9bf00ab8977a7132e4232db08a61077e4f8e48df
happy-sonday <mrsmiler1106@gmail.com>
2021-12-25 01:53:39 +0900

    캐러셀 및 인디케이터 UI

--- flutter_haplix/lib/screen/home_screen.dart
+++ flutter_haplix/lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_haplix/model/model_movie.dart';
+import 'package:flutter_haplix/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,6 +10,32 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    Movie.fromMap({
+      'title': '그해 우리는',
+      'keyword': '사랑/청춘/로맨스',
+      'poster': 'we_were.jpg',
+      'like': false,
+    }),
+    Movie.fromMap({
+      'title': '그해 우리는',
+      'keyword': '사랑/청춘/로맨스',
+      'poster': 'we_were.jpg',
+      'like': false,
+    }),
+    Movie.fromMap({
+      'title': '그해 우리는',
+      'keyword': '사랑/청춘/로맨스',
+      'poster': 'we_were.jpg',
+      'like': false,
+    }),
+    Movie.fromMap({
+      'title': '그해 우리는',
+      'keyword': '사랑/청춘/로맨스',
+      'poster': 'we_were.jpg',
+      'like': false,
+    }),
+  ];
   @override
   void initState() {
     super.initState();
@@ -20,7 +48,19 @@ class _HomeScreenState extends State<HomeScreen> {
     //     child: Text("real home"),
     //   ),
     // );
-    return TopBar();
+    return ListView(
+      children: [
+        // 스택원리로 제일 먼저 오는게 제일 마지막에 위치해야 한다.
+        Stack(
+          children: [
+            CarouselImage(
+              movies: movies,
+            ),
+            const TopBar(),
+          ],
+        )
+      ],
+    );
   }
 }
 
