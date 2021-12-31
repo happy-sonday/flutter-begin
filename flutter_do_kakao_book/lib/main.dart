cb355515b6cfba822f08e7cce82d41d387613afb
happy-sonday <mrsmiler1106@gmail.com>
2021-12-31 12:28:31 +0900

    도서 검색 기능 추가

--- flutter_do_kakao_book/lib/main.dart
+++ flutter_do_kakao_book/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -36,59 +37,82 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String result = '';
   List? data;
+  TextEditingController? _editingController;
+  ScrollController? _scrollController;
+  int page = 1;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     data = new List.empty(growable: true);
+    _editingController = new TextEditingController();
+    _scrollController = new ScrollController();
+
+    _scrollController!.addListener(() {
+      if (_scrollController!.offset >=
+              _scrollController!.position.maxScrollExtent &&
+          !_scrollController!.position.outOfRange) {
+        print("bottom");
+        page++;
+        getJSONData();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('http Example'),
+        title: TextField(
+          controller: _editingController,
+          style: TextStyle(color: Colors.white),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(hintText: "검색어를 입력하세요"),
+        ),
       ),
-      body: Container(
-          child: Center(
-              child: data!.length == 0
-                  ? const Text(
-                      "데이터가 없습니다",
-                      style: TextStyle(fontSize: 25),
-                      textAlign: TextAlign.center,
-                    )
-                  : ListView.builder(itemBuilder: (context, index) {
-                      return Card(
-                        child: Container(
-                          child: Row(
-                            children: <Widget>[
-                              Image.network(
-                                data![index]['thumbnail'],
-                                height: 100,
-                                width: 100,
-                                fit: BoxFit.contain,
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: <Widget>[
-                                  Container(
-                                    width:
-                                        MediaQuery.of(context).size.width - 150,
-                                    child: Text(
-                                      data![index]['title'].toString(),
-                                      textAlign: TextAlign.center,
-                                    ),
+      body: Center(
+          child: data!.length == 0
+              ? const Text(
+                  "데이터가 없습니다",
+                  style: TextStyle(fontSize: 25),
+                  textAlign: TextAlign.center,
+                )
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Container(
+                        child: Row(
+                          children: <Widget>[
+                            Image.network(
+                              data![index]['thumbnail'],
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.contain,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width - 150,
+                                  child: Text(
+                                    data![index]['title'].toString(),
+                                    textAlign: TextAlign.center,
                                   ),
-                                  Text(data![index]['authors'].toString()),
-                                  Text(data![index]['sela_price'].toString()),
-                                  Text(data![index]['status'].toString()),
-                                ],
-                              )
-                            ],
-                          ),
+                                ),
+                                Text(data![index]['authors'].toString()),
+                                Text(data![index]['sela_price'].toString()),
+                                Text(data![index]['status'].toString()),
+                              ],
+                            )
+                          ],
                         ),
-                      );
-                    }))),
+                      ),
+                    );
+                  },
+                  controller: _scrollController,
+                  itemCount: data!.length)),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           //var url = 'http://www.google.com';
@@ -96,7 +120,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // setState(() {
           //   result = response.body;
           // });
-
+          page = 1;
+          data!.clear();
           getJSONData();
         },
         child: const Icon(Icons.file_download),
@@ -105,7 +130,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<String> getJSONData() async {
-    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query=doit';
+    var url =
+        'https://dapi.kakao.com/v3/search/book?target=title&page=$page&query=${_editingController!.value.text}';
 
     var response = await http.get(Uri.parse(url),
         headers: {"Authorization": "KakaoAK ${dotenv.env['kakaoAPI']}"});
