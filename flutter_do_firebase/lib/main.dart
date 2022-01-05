27dcb8ae7342cd65fc227fbcbed972f01873bfe9
happy-sonday <mrsmiler1106@gmail.com>
2022-01-05 16:56:50 +0900

    파이어베이스 앱푸시 기능 추가

--- flutter_do_firebase/lib/main.dart
+++ flutter_do_firebase/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_do_firebase/memo_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_app.dart';
 
@@ -25,7 +27,57 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       //home: FirebaseApp(analytics: analytics, observer: observer),
-      home: const MemoPage(),
+      //home: const MemoPage(),
+      home: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          // 에러발생시 출력 위젯
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Error"),
+            );
+          }
+
+          // 선언 완료 후 표시할 위젯
+          if (snapshot.connectionState == ConnectionState.done) {
+            _initFirebaseMessagin(context);
+            _getToken();
+            return const MemoPage();
+          }
+
+          // 선언되는 동안 표시할 위젯
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
     );
+  }
+
+  _initFirebaseMessagin(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+      print(event.notification!.title);
+      print(event.notification!.body);
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                title: const Text("알림"),
+                content: Text(event.notification!.body!),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Ok"))
+                ],
+              ));
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage event) {});
+  }
+
+  _getToken() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+    print("messaging.getToken(),${await messaging.getToken()}");
   }
 }
