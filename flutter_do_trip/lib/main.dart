690a82330c2ed32f0fe782124bc4fd1bdd7c7869
happy-sonday <mrsmiler1106@gmail.com>
2022-01-08 22:50:50 +0900

    메인 페이지 및 탭메뉴 퍼블리싱

--- flutter_do_trip/lib/main.dart
+++ flutter_do_trip/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_do_trip/model/page/login_page.dart';
+import 'package:flutter_do_trip/model/page/main_page.dart';
 import 'package:flutter_do_trip/model/page/sign_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -24,6 +25,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const LoginPage(),
         '/sign': (context) => const SignPage(),
+        '/main': (context) => const MainPage(),
       },
     );
   }
---
+++ flutter_do_trip/lib/model/page/main_page.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_do_trip/model/page/favorite_page.dart';
+import 'package:flutter_do_trip/model/page/map_page.dart';
+import 'package:flutter_do_trip/model/page/setting_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  TabController? controller;
+  FirebaseDatabase? _database;
+  DatabaseReference? reference;
+  String? _dataBaseURL = dotenv.env['DB_URL'];
+  String? id;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+    _database = FirebaseDatabase(databaseURL: _dataBaseURL);
+    reference = _database!.reference();
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // 로그인할 때 전달받은 아이디값을 저장, 각 페이지로 이동할 때마다 이 값을 전달
+    id = ModalRoute.of(context)!.settings.arguments as String?;
+    return Scaffold(
+        body: TabBarView(
+          children: [MapPage(), FavoritePage(), SettingPage()],
+          controller: controller,
+        ),
+        bottomNavigationBar: TabBar(
+          tabs: const [
+            Tab(
+              icon: Icon(Icons.map),
+            ),
+            Tab(
+              icon: Icon(Icons.star),
+            ),
+            Tab(
+              icon: Icon(Icons.settings),
+            ),
+          ],
+          labelColor: Colors.amber,
+          indicatorColor: Colors.deepOrangeAccent,
+          controller: controller,
+        ));
+  }
+}
